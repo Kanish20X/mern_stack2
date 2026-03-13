f561667cedf01b70eb4fda18ca08637da987f42f
Kanish <mkanish2004@gmail.com>
2026-03-13 09:59:14 +0530

    updated

--- index.js
+++ index.js
@@ -40,11 +40,10 @@ app.use("/store", storeRouter);
 app.use("/", authenticateAdmin, homeRouter);
 app.use("/products", authenticateAdmin, productRouter);
 app.use("/users", authenticateAdmin, userRouter);
-const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
     await dbConnect();
-    app.listen(PORT, () => {
+    app.listen(5000, () => {
         console.log("Server Started");
     });
 };
